e4b735edbad1933019edd183f5cb0d2238c3a50a
Yeuk Hon Wong <yeukhon@acm.org>
2014-08-29 00:54:51 -0400

    Add dotfile installer

--- README.md
+++ README.md
@@ -1,4 +1,15 @@
 dotfiles
 ========
 
-My dotfiles
+Usage:
+
+```
+./dotfile.py
+
+Install dotfiles
+
+optional arguments:
+  -h, --help  show this help message and exit
+  --mac       Install dotfiles on Mac.
+  --linux     Install dotfiles on Linux.
+```
---
+++ dotfiles.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+
+import argparse
+import getpass
+import os
+import sys
+
+try:
+    from urllib.request import urlretrieve
+except ImportError:
+    from urllib import urlretrieve
+
+COMMON_FILES = [
+    ".gitconfig",
+    ".hgrc",
+    ".pythonrc",
+    ".vimrc"
+]
+
+MAC_FILES = [
+    (".bash_profile.mac", ".bash_profile"),
+]
+
+LINUX_FILES = [
+    ".bash_aliases"
+]
+
+URL = "https://github.com/yeukhon/dotfiles/blob/master/"
+
+def file_maps(files):
+    maps = {}
+    #home_dir = os.path.expanduser("~")
+    home_dir = "/tmp/folder"
+    for f in files:
+        if isinstance(f, tuple):
+            _url = URL + f[0]
+            maps[_url] = os.path.join(home_dir, f[1])
+        else:
+            _url = URL + f
+            maps[_url] = os.path.join(home_dir, f)
+    return maps
+
+def download_files(files):
+    for link, fpath in files.items():
+        print("Downloading {}".format(link.split("/")[-1]))
+        local_path, headers = urlretrieve(link, filename=fpath, reporthook=f)
+
+def retrieve(is_mac=False, is_linux=False):
+    if is_mac:
+        files = file_maps(MAC_FILES + COMMON_FILES)
+    elif is_linux:
+        files = file_maps(LINUX_FILES + COMMON_FILES)
+
+    download_files(files)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Install dotfiles")
+    parser.add_argument("--mac", action="store_true",
+                       help="Install dotfiles on Mac.")
+    parser.add_argument("--linux", action="store_true",
+                       help="Install dotfiles on Linux.")
+
+    if len(sys.argv) == 1:
+        parser.print_help()
+    else:
+        args = parser.parse_args()
+        print(args)
+        if args.mac:
+            retrieve(is_mac=True)
+        elif args.linux:
+            retrieve(is_linux=True)
+        else:
+            sys.exit("Meh I don't get it. RTFM")
